21e75c3b4da10819245d5251202dd108e8cedf11
elijahp1595 <pina.elijah@gmail.com>
2026-06-13 01:59:53 +0000

    I have finished mini 7

--- week4-exercises/mini07-alert-service/src/main/java/edu/norcocollege/cis18b/weekx/mini07/AlertValidator.java
+++ week4-exercises/mini07-alert-service/src/main/java/edu/norcocollege/cis18b/weekx/mini07/AlertValidator.java
@@ -3,5 +3,16 @@ package edu.norcocollege.cis18b.weekx.mini07;
 public class AlertValidator {
     public void validate(Alert alert) throws InvalidAlertException {
         // TODO: Validate alert fields.
+        if (alert == null) {
+            throw new InvalidAlertException("Alert cannot be null.");
+        }
+
+        if (alert.message() == null || alert.message().isBlank()) {
+            throw new InvalidAlertException("Alert message cannot be blank.");
+        }
+
+        if (alert.level() == null) {
+            throw new InvalidAlertException("Alert level cannot be null.");
+        }
     }
 }
--- week4-exercises/mini07-alert-service/src/main/java/edu/norcocollege/cis18b/weekx/mini07/InMemoryAlertRepository.java
+++ week4-exercises/mini07-alert-service/src/main/java/edu/norcocollege/cis18b/weekx/mini07/InMemoryAlertRepository.java
@@ -9,6 +9,7 @@ public class InMemoryAlertRepository implements AlertRepository {
     @Override
     public void save(Alert alert) throws AlertStorageException {
         // TODO: Save the alert or simulate a failure.
+        alerts.add(alert);
     }
 
     public List<Alert> findAll() {
